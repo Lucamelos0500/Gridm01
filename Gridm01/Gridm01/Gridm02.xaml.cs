a361be33f0a6e68c9efb33e9b25ce9243d9fc24a
lucamelos0500 <lucamelos0500@gmail.com>
2021-09-20 11:52:29 -0300

    Adicionar arquivos de projeto.

---
+++ Gridm01/Gridm01/Gridm02.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Gridm01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Gridm02 : ContentPage
+    {
+        public Gridm02()
+        {
+            InitializeComponent();
+        }
+
+        private void btVoltar(object sender, EventArgs e)
+        {
+            Navigation.PopToRootAsync();
+        }
+    }
+}
